6e808cca9545bfb7e9b62a9927933e5a1cacbb05
Andres-Alexander-Silva <andresalexanderss@ufps.edu.co>
2023-04-13 17:54:39 -0500

    Validacion Lista

--- BECL_Login/models.py
+++ BECL_Login/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 
-class User(models.Model):
-    codigo = models.CharField(max_length=10,primary_key=True)
-    nombre = models.CharField(max_length=200)
+class Usuarios (models.Model):
+    codigo = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=150)
     facultad = models.CharField(max_length=150)
-    email = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150)
     usuario = models.CharField(max_length=20)
     password = models.CharField(max_length=300)
     
-    def password_encript(self):
-        password_nueva = make_password(self.password)
+    def password_encript(self, password):
+        password_nueva = make_password(password)
         self.password = password_nueva
     
     def password_check(self, password):
@@ -18,4 +18,4 @@ class User(models.Model):
         return is_valid
     
     def __str__(self):
-        return self.username + ":" + self.password 
+        return self.usuario
--- BECL_Login/views.py
+++ BECL_Login/views.py
@@ -1,20 +1,25 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.conf import settings
-from .models import User
+from .models import Usuarios
 import jwt
+import json
 
 @csrf_exempt
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        body = json.loads(request.body.decode('utf-8'))
+        username = body.get("username")
+        password = body.get("password")
+        print(username)
         try:
-            user = User.objects.get(username=username)
+            user = Usuarios.objects.get(usuario=username)
+            print(user)
             if user is not None and user.password_check(password):
-                token = jwt.encode({'user_id': user.id}, settings.SECRET_KEY, algorithm='HS256')
-                return JsonResponse({'token': token.decode('utf-8')})
+                token = jwt.encode({'user_id': user.codigo, 'user_name': user.nombre,
+                                    'user_email': user.email, 'user_faculty': user.facultad}, settings.SECRET_KEY, algorithm='HS256')
+                return JsonResponse({'token': token})
             else:
                 return JsonResponse({'error': 'Credenciales Invalidas'})
-        except User.DoesNotExist:
+        except Usuarios.DoesNotExist:
             return JsonResponse({'error': 'El Usuario no existe'})
