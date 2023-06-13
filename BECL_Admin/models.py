b18b397d1af370549ec8157d2df22b609a802be7
Andres-Alexander-Silva <andresalexanderss@ufps.edu.co>
2023-06-13 10:18:56 -0500

    Se creo la tabla rol y se realizaron los respectivos asignamientos

--- BECL_Admin/models.py
+++ BECL_Admin/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Estado(models.Model):
     descripcion = models.CharField(max_length=100, null=True)
+    
+    def __str__(self):
+        return self.descripcion
--- BECL_Login/models.py
+++ BECL_Login/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 
+class Rol_Usuario(models.Model):
+    descripcion = models.CharField(max_length=100, null=True)
+    
+    def __str__(self):
+        return self.descripcion
+
 class Usuarios (models.Model):
     codigo = models.CharField(max_length=10, primary_key=True)
     nombre = models.CharField(max_length=150)
     facultad = models.CharField(max_length=150)
     email = models.EmailField(max_length=150)
+    rol = models.ForeignKey(Rol_Usuario, on_delete=models.CASCADE, null=True, blank=True, related_name="rol_usuario")
     usuario = models.CharField(max_length=20)
     password = models.CharField(max_length=300)
     
