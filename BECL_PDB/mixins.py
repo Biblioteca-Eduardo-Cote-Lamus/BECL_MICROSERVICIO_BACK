77553d20f2038207ed491b5fb08ced703c44778d
angeldev07 <angelgaran20@gmail.com>
2023-10-01 00:59:23 -0500

    feat(BECL_PDB): Se crea un mixin para manejar los servicios de google a traves de la api.

--- BECL_PDB/mixins.py
+++ BECL_PDB/mixins.py
@@ -1,5 +1,4 @@
 import os
-import datetime
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -8,43 +7,68 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
 
-class GoogleAPIMixin: 
-
-    SCOPES = [
+class GoogleAPIMixin:
+    __SCOPES = [
         "https://www.googleapis.com/auth/calendar",
         "https://www.googleapis.com/auth/drive",
     ]
 
     def __init__(self) -> None:
-        self.__credential = self.__getCredentials()
-        self.__service = None
+        self.service = None
 
-    def __init_service(self, service: str) :
-        self.__service = build(service, "v3", credentials=self.__credential)
+    def init_service(self, service: str):
+        self.service = build(service, "v3", credentials=self.__getCredentials())
 
     def __getCredentials(self):
-    # Si modifica este scopes, borra el archivo token.json
+        # Si modifica este scopes, borra el archivo token.json
         creds = None
         # El archivo token.json almacena los tokens de acceso y actualización del usuario, y es
         # creado automáticamente cuando el flujo de autorización se completa por primera vez
         # tiempo.
         if os.path.exists("token.json"):
-            creds = Credentials.from_authorized_user_file("token.json", self.SCOPES)
+            creds = Credentials.from_authorized_user_file("token.json", self.__SCOPES)
         # Si no hay credenciales (válidas) disponibles, permita que el usuario inicie sesión.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", self.SCOPES)
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", self.__SCOPES
+                )
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
         return creds
-    
+
     def upload_doc(self):
         pass
 
+    def get_list_events(self, start, end):
+        events_result = (
+            self.service.events()
+            .list(
+                calendarId="primary",
+                timeMin=start,
+                timeMax=end,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        return events_result.get("items", [])
+
+    def insert_event(self, calendar_body):
+        try:
+            # self.init_service('calendar')
+            self.service.events().insert(calendarId="primary", body=calendar_body).execute()
+            return True
+        except Exception as e:
+            raise e
+        
+
+    def update_event(self):
+        pass
 
 
 """
@@ -75,5 +99,3 @@ try:
 except Exception as err:
             raise err
 """
-
-
